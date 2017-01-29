92c3e06c8d730673a39ae5e79e8912e9594126b8
kyjo2014 <467346087@qq.com>
2017-01-29 23:36:19 +0800

    add config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueResource from 'vue-resource'
+import goods from './components/goods/goods'
+import ratings from './components/ratings/ratings'
+import seller from './components/seller/seller'
+
+import ''
 
 /* eslint-disable no-new */
 new Vue({
