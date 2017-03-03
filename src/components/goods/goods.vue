f321f3ac39d9bb92f3d14407a5407bb01a6b69cd
kyjo2014 <467346087@qq.com>
2017-03-03 23:47:10 +0800

    增加good外层css

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -10,5 +10,12 @@
 
 </script>
 <style lang="stylus" scoped>
-
+    .goods
+        display flex
+        position absolute
+        top 174px
+        width 100%
+        .menu-wrapper
+        .food-wrapper
+        
 </style>
